f016b7a930a958b23467223598eb8cd167f7ecbd
apelqvistj <apelqvistj@users.noreply.github.com>
2020-02-21 07:33:57 +0100

    Menu component added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AlgorithmMenu />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlgorithmMenu from './components/AlgorithmMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlgorithmMenu
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/components/AlgorithmMenu.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="menuwrapper">
+        <span class="cta">Pick a sorting algorithm!</span>
+        <div class="algomenu">
+            <ul>
+                <li>Bubble sort</li>
+                <li>Merge sort</li>
+                <li>Insertion sort</li>
+                <li>Quick sort</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'AlgorithmMenu'
+    }
+</script>
+
+<style scoped>
+    ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 10px;
+    }
+    li {
+    display: inline-block;
+    margin: 0 15px;
+    }
+    .menuwrapper {
+        display: flex;
+        justify-content: space-evenly;
+        padding: 10px 100px 10px 100px;
+        background-color: #233559;
+        color: #E09F29;
+    }
+    .cta {
+        font-size: 1.5em;
+    }
+    .algomenu {
+        display: inline-block;
+    }
+</style>
